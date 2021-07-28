35ec6c8be6b312ca27b7747106bb312128913757
Febri Nur Rohman <vanderfarrel@gmail.com>
2021-07-28 23:50:47 +0700

    membuat map

--- src/App.js
+++ src/App.js
@@ -14,14 +14,26 @@ export default function App() {
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
-            style: 'mapbox://styles/vanderfarrel/ckql900vk1z5q18n9y8hmyrns',
+            style: 'mapbox://styles/mapbox/streets-v11',
             center: [lng, lat],
             zoom: zoom
         });
     });
 
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+    });
+
     return (
         <div>
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
             <div ref={mapContainer} className="map-container" />
         </div>
     );
